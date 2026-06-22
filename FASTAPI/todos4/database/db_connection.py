cd325c859efef083b3e2d0009754c69f2112b5bd
Jong Cheon Park <simpleego@gmail.com>
2026-06-22 12:47:55 +0900

    Update db_connection.py

--- FASTAPI/todos4/database/db_connection.py
+++ FASTAPI/todos4/database/db_connection.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 # 데이터베이스 연결 정보 설정
 DATABASE_URL = "mysql+pymysql://root:fastapi@localhost:3306/fastapi_db"
+# [DB_종류]+[드라이버]://[사용자명]:[비밀번호]@[호스트_주소]:[포트번호]/[DB_이름]
 
 # 엔진 생성
 engine = create_engine(DATABASE_URL, echo=True)
